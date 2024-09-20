dfb437120f8e4d40ca34bbf092a8a0d125becbec
Angelina Kutuzova <a.kutuzova11@gmail.com>
2024-09-20 12:32:16 +0200

    add a function that creates new tasks on click and add buttons of completion and removal next to the tasks

--- dev.js
+++ dev.js
@@ -9,4 +9,25 @@
             completed: true,
         },
     ];
-    
+    const addNewItem = (newItemContent) => {
+        tasks.push({
+          content: newItemContent,
+        });
+        render();
+      }
+      const render = () => {
+        let htmlString = "";
+        for(const task of tasks) {
+            htmlString += `
+            <li class="todoTasks__item ${task.completed ? "todoTasks__item--toggleCompleted" : ""}">
+            <button class="js-toggleCompleted todoTasks__button">${task.completed ? "✔" : ""}
+            </button>
+            <span class="${task.completed ? "todoTasks__item--toggleCompleted" : ""}">${task.content}
+            </span>
+            <button class="js-remove todoTasks__button todoTasks__button--deleted">🗑</button>
+            </li>
+            `;
+        }
+        
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+        
