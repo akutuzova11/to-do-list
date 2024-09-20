7d8753c709fa0a729b3ed30f8f4f57ffcdc2aff5
Angelina Kutuzova <a.kutuzova11@gmail.com>
2024-09-20 12:27:50 +0200

    add sample tasks visible in the to-do lists

---
+++ dev.js
@@ -0,0 +1,12 @@
+{
+    const tasks = [
+        {
+            content: "spacer w parku",
+            completed: false,
+        },
+        {
+            content: "yoga o 19:30",
+            completed: true,
+        },
+    ];
+    
